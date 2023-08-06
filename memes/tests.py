9695a16558be4f5536cafb61710399c54e00547b
KimDuHong <art970@naver.com>
2023-08-06 19:34:02 +0900

    change test code

--- memes/tests.py
+++ memes/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+
+# Shop/product GET TEST
+@pytest.mark.django_db
+class TestofTest:
+    def test_pytest(self):
+        assert 1 == 1
