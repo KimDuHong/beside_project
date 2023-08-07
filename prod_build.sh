ddf4da7e99b51b6bc17e28117f842a2346490dd4
KimDuHong <art970@naver.com>
2023-08-07 17:18:19 +0900

    Add prod container

---
+++ prod_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "==> Migration 파일 생성"
+yes | python manage.py makemigrations
+
+echo "==> Migrate 실행"
+python manage.py migrate 
+
+echo "==> Default 데이터 세팅"
+python manage.py default_tag
+
+echo "==> 배포!"
+
+echo "Deploy Gunicorn"
+echo "==> collect static 실행"
+python manage.py collectstatic --noinput
+gunicorn config.wsgi:application --bind 0.0.0.0:8001
