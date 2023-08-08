0f299b0551ecdeae8cb98d4720d6a94c2813d8ac
KimDuHong <art970@naver.com>
2023-08-09 04:15:57 +0900

    add postgreSQL for prod server

--- config/settings.py
+++ config/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
